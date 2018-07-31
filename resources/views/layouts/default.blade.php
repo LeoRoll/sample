bb6328ca3ff909cf32847a29b2528bca4bf274ba
LeoRoll <1455212235@qq.com>
2018-07-31 17:36:41 +0000

    ok

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel 系统复习</title>
+    <title>@yield('title', 'Sample App') - Laravel 系统复习KO</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
