87f8de6d3833677f6b2aa1116772f1141c8214dd
LeoRoll <1455212235@qq.com>
2018-08-02 09:38:46 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,7 @@
 
     <div class="container">
         <div class="col-md-offset-1 col-md-10"></div>
+            @include('shared._messages')
             @yield('content')
             @include('layouts._footer')
     </div>
