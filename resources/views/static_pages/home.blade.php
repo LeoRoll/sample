c412197d49c42ab72c598917c1b4609381b5f140
LeoRoll <1455212235@qq.com>
2018-08-09 02:43:52 +0000

    something

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -20,10 +20,10 @@
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+        你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.5">Laravel 复习教程</a> 的项目主页。
       </p>
       <p>
-        一切，将从这里开始。
+        扬帆，起航
       </p>
       <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
